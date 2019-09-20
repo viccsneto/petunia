576786ade0a25c33c6f7bd71337fdffef797d79a
Vicente Neto <vic.cs.neto@gmail.com>
2019-09-20 10:38:39 -0300

    Recover old simpler test case

--- test/PetuniaTests.cpp
+++ test/PetuniaTests.cpp
@@ -9,6 +9,38 @@ TEST(PetuniaTests, PetuniaMessageSend) {
   std::promise<bool> promise;
   std::future<bool> future = promise.get_future();
 
+  Petunia::Petunia *server = new Petunia::Petunia(channel, Petunia::ConnectionRole::Server);
+  Petunia::Petunia *client = new Petunia::Petunia(channel);
+ 
+  client->AddListener(message_type, [&](const Petunia::Message &message) {
+    EXPECT_STREQ(message_content.c_str(), message.GetText());
+    client->SendMessage(new Petunia::Message(message_type, message_content));
+  });
+
+  server->AddListener(message_type, [&](const Petunia::Message &message) {
+    EXPECT_STREQ(message_content.c_str(), message.GetText());
+    printf("No more!!!\n");
+    printf("Received: %s %llu 0x%p\n", message.GetText(), message.GetDataSize(), message.GetData());
+    promise.set_value(true);
+  });
+
+  server->SendMessage(new Petunia::Message(message_type, message_content));
+  
+
+  printf("Waiting...");
+  future.wait();
+  
+  delete client;
+  delete server;
+}
+
+TEST(PetuniaTests, PetuniaManyMessagesSend) {
+  std::string channel = "test";
+  std::string message_type = "test_message";
+  std::string message_content = "content of the message";
+  std::promise<bool> promise;
+  std::future<bool> future = promise.get_future();
+
   int count = 1024;
   size_t message_size = 1024 * 1024 * 2; // 2MB
   
