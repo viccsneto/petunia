10c2d112daf53720ab75615ff778559b02cd1b79
Vicente Neto <vic.cs.neto@gmail.com>
2019-09-20 10:34:00 -0300

    Makes a more meaningful test

--- test/PetuniaTests.cpp
+++ test/PetuniaTests.cpp
@@ -9,9 +9,20 @@ TEST(PetuniaTests, PetuniaMessageSend) {
   std::promise<bool> promise;
   std::future<bool> future = promise.get_future();
 
+  int count = 1024;
+  size_t message_size = 1024 * 1024 * 2; // 2MB
+  
+  if (message_size < message_content.size()) {
+    message_content.resize(message_size);
+  }
+  else {
+    message_content.resize(message_size, 'A');
+  }
+
+
   Petunia::Petunia *server = new Petunia::Petunia(channel, Petunia::ConnectionRole::Server);
   Petunia::Petunia *client = new Petunia::Petunia(channel);
- 
+  
   client->AddListener(message_type, [&](const Petunia::Message &message) {
     EXPECT_STREQ(message_content.c_str(), message.GetText());
     client->SendMessage(new Petunia::Message(message_type, message_content));
@@ -19,13 +30,15 @@ TEST(PetuniaTests, PetuniaMessageSend) {
 
   server->AddListener(message_type, [&](const Petunia::Message &message) {
     EXPECT_STREQ(message_content.c_str(), message.GetText());
-    printf("No more!!!\n");
-    printf("Received: %s %llu 0x%p\n", message.GetText(), message.GetDataSize(), message.GetData());
-    promise.set_value(true);
+    if (--count <= 0) {
+      printf("No more!!!\n");
+      promise.set_value(true);
+    } else {
+      server->SendMessage(new Petunia::Message(message_type, message_content));
+    }
   });
 
   server->SendMessage(new Petunia::Message(message_type, message_content));
-  
 
   printf("Waiting...");
   future.wait();
