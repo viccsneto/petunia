3c1c10749eb82301d35038c34d40c60ff2c10ff4
Vicente Correa da Silva Neto <vic.cs.neto@gmail.com>
2019-10-02 15:25:19 -0300

    Fixes misuse of private field m_connection_role

--- src/ipc_medium.cpp
+++ src/ipc_medium.cpp
@@ -1,6 +1,18 @@
 #include <petunia/ipc_medium.h>
 namespace Petunia 
 {
+
+  IPCMedium::IPCMedium(std::string &channel, ConnectionRole connection_role /*= ConnectionRole::Auto*/) : m_channel(channel)
+    , m_connection_role(connection_role)
+  {
+
+  }
+
+  IPCMedium::~IPCMedium()
+  {
+
+  }
+
   ConnectionRole IPCMedium::GetConnectionRole()
   {
     return m_connection_role;
