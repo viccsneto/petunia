4f2b588c6fb25c84ace094c72905bbf7927accf1
Vicente Correa da Silva Neto <vic.cs.neto@gmail.com>
2020-01-21 20:09:50 -0300

    Removes excluded header file from CMakeLists.txt
    Adds fallback cmake option on build_msvc64.bat

--- build_msvc64.bat
+++ build_msvc64.bat
@@ -3,3 +3,4 @@ cd %~dp0
 if not exist build md build
 cd build
 cmake -G "Visual Studio 16 2019" -A Win64 -DCMAKE_BUILD_TYPE=Release ..
+if errorlevel 1 cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release ..
